6e5b67eb9e37b9275db90f7b5963acfef1eeaa23
unknown <bigboy29@freenet.de>
2011-11-16 21:12:49 +0100

    Updated license information

--- AttackAlert.php
+++ AttackAlert.php
@@ -2,16 +2,14 @@
 /**
  * Piwik - Open source web analytics
  * 
- * @link http://piwik.org
- * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
- * @version $Id: $
- * 
  * @category Piwik_Plugins
  * @package Piwik_AttackAlert
  */
 
 /**
+ * AttackAlert
  *
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
  * @package Piwik_AttackAlert
  * @extends Piwik_Plugin
  */
--- Controller.php
+++ Controller.php
@@ -2,10 +2,6 @@
 /**
  * Piwik - Open source web analytics
  * 
- * @link http://piwik.org
- * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
- * @version $Id: $
- * 
  * @category Piwik_Plugins
  * @package Piwik_AttackAlert
  */
@@ -13,7 +9,10 @@
 require_once PIWIK_INCLUDE_PATH . '/plugins/UserSettings/functions.php';
 
 /**
+ * AttackAlert
  *
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * 
  * @package Piwik_AttackAlert
  * @extends Piwik_Controller
  */
